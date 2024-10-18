13b88bd6de1a16d5e2ab8b45afb99d279c396921
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2024-10-18 07:32:01 +0530

    backend struct

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"sync"
 )
 
 //Backend
-
+//backend interface
 type Backend interface{
 	SetAlive(bool)
 	IsAlive() bool
 	GetUrl() *url.URL
 	GetActiveConnections() int
 	Serve(http.ResponseWriter, *http.Request)
+}
+
+//backend struct
+type backend struct {
+	url *url.URL
+	alive bool
+	mux sync.RWMutex
+	connections int
+	reverseProxy *httputil.ReverseProxy
 }
